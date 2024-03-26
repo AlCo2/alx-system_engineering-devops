18408d494231ad6f49c6d0ec386a48de62a7904a
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-26 18:16:40 +0000

    change task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,20 @@
 # set up nginx server
-
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
+package { 'nginx':
+  ensure => installed,
 }
 
-packege { 'nginx':
-	ensure => installed,
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-	ensure => file,
-	content => "Hello World!"
+  content => 'Hello World!',
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
-}
-
-exec {'error_404':
-	command => 'sudo sed -i '25i\	error_page 404 /error_404.html;' /etc/nginx/sites-available/default'
-	provider => 'shell'
-}
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
