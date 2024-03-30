427c1ad0b87915b0dbe477e59519d4e98e0e3abe
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-30 06:06:55 +0000

    advenced task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+# set up nginx server
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'add_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By "<%= hostname %>"',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
