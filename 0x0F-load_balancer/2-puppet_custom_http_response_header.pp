4c9ceedebf4b9c38c0b7aab1804a8ab71402a240
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-30 06:11:50 +0000

    change everything

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,14 +1,8 @@
 # set up nginx server
-package { 'nginx':
-  ensure => installed,
-}
-
-exec {'add_header':
-	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-	provider => 'shell'
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
