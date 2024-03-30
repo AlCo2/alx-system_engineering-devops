cc559bdb3b50738930f2a0b31faf29d9dcabf853
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-30 06:08:35 +0000

    update hostname

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,7 +7,7 @@ file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By "<%= hostname %>"',
+  line   => 'add_header X-Served-By "<%= fqdn %>"',
 }
 
 file { '/var/www/html/index.html':
