54b7101887cca0ffd10d72de3eb6218461fa0543
Alco2 <abdou.lahboub.wwe@gmail.com>
2024-06-03 11:35:30 +0100

    another solution

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Manifest to fix apache configuration
+exec { 'fix config file':
+  command =>  "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin'
 }
